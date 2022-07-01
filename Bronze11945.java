dae91bbb7cf25eac4dbc60c3b4be4bf020744281
Wish-baek <dnjs0236@gmail.com>
2022-07-01 15:07:45 +0900

    22.06.30

--- Bronze11945.java
+++ Bronze11945.java
@@ -1,9 +1,25 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.IOException;
 
-public class Bronze11945 {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
-
+public class Bronze11945{
+    public static void main(String args[]) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in)); 
+        String r = bf.readLine();
+        String[] rr = r.split(" ");
+        int a = Integer.parseInt(rr[0]);
+        int b = Integer.parseInt(rr[1]);
+        
+        for(int i = 0; i < a; i++) {
+        	String arr = bf.readLine();
+        	for(int j = b-1; j >= 0; j--) {
+        		System.out.print(arr.charAt(j));
+        	}
+        	System.out.println("");
+        	
+        }
+        
+    }
 }
--- Bronze15726.java
+++ Bronze15726.java
@@ -1,9 +1,22 @@
+import java.io.BufferedReader;
+import java.util.Scanner;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
 
 public class Bronze15726 {
+	
+	public static void main(String[] args) throws IOException{
+		Scanner sc = new Scanner(System.in);
+		double a = sc.nextDouble();
+		double b = sc.nextDouble();
+		double c = sc.nextDouble();
+		
+		double x = a/b*c;
+		double y = a*b/c;
+		
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		System.out.println((int)(Math.max(x, y)));
 	}
 
 }
