6ffbfcd96f221e8c65e010a203b55be196c391ce
Wish-baek <dnjs0236@gmail.com>
2022-07-03 16:06:25 +0900

    22.07.03

--- Bronze1547.java
+++ Bronze1547.java
@@ -12,8 +12,8 @@ public class Bronze1547 {
 		for(i = 0; i < n; i++) {
 			int a = sc.nextInt();
 			int b = sc.nextInt();
-			int ax = Arrays.asList(arr).indexOf(a); //a 위치 검
-			int bx = Arrays.asList(arr).indexOf(b); //b 위치 검
+			int ax = Arrays.asList(arr).indexOf(a); //a 위치 검색 
+			int bx = Arrays.asList(arr).indexOf(b); //b 위치 검색 
 			arr[ax] = b; //a가 있는 위치 값을 b로 변경 
 			arr[bx] = a; //b가 있는 위치 값을 a로 변경 
 			
