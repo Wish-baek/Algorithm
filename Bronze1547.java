0364c57dc8eb4bd1f22222d481a83d90cebe323c
Wish-baek <dnjs0236@gmail.com>
2022-07-03 15:52:58 +0900

    22.07.03

---
+++ Bronze1547.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+import java.util.Arrays;
+import java.math.BigInteger;
+public class Bronze1547 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		
+	}
+}
