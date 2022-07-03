14cc60d92b17d50f002b936f8dc19063d51a1e06
Wish-baek <dnjs0236@gmail.com>
2022-07-03 16:09:41 +0900

    22.07.03

---
+++ Bronze23825.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Bronze23825 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		int s = sc.nextInt();
+		int a = sc.nextInt();
+		
+		System.out.println(Math.min(s/2, a/2));
+		
+	}
+
+}
