d8c11380acb5af481177dc369948fc44ec198a74
Wish-baek <dnjs0236@gmail.com>
2022-07-03 16:04:58 +0900

    22.07.03

--- Bronze1547.java
+++ Bronze1547.java
@@ -1,11 +1,26 @@
 import java.util.Scanner;
 import java.util.Arrays;
-import java.math.BigInteger;
 public class Bronze1547 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		Integer[] arr = {1, 2, 3};
 		
+		int i, j;
+		for(i = 0; i < n; i++) {
+			int a = sc.nextInt();
+			int b = sc.nextInt();
+			int ax = Arrays.asList(arr).indexOf(a); //a 위치 검
+			int bx = Arrays.asList(arr).indexOf(b); //b 위치 검
+			arr[ax] = b; //a가 있는 위치 값을 b로 변경 
+			arr[bx] = a; //b가 있는 위치 값을 a로 변경 
+			
+		}
+		
+		System.out.println(arr[0]);
 	}
+
 }
+
