b99226767689d37c4557a1a36991dc1ae8322b98
Wish-baek <dnjs0236@gmail.com>
2022-07-06 15:57:30 +0900

    22.07.06

---
+++ Bronze4470.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class Bronze4470 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		
+		String t = sc.nextLine();
+		int T = Integer.parseInt(t);
+		for(int i = 1; i <= T; i++) {
+			String a = sc.nextLine();
+			System.out.println(i + ". " + a);
+		}
+		
+		
+	}
+
+}
