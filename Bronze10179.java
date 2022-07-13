325ac6f91558087d0f981e50dcaf392fc92c549a
Wish-baek <dnjs0236@gmail.com>
2022-07-13 13:46:24 +0900

    22.07.13

---
+++ Bronze10179.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Bronze10179 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		for(int i = 0; i < n; i++) {
+			double a = sc.nextDouble();
+			System.out.printf("$%.2f",a/100*80);
+			System.out.println();
+		}
+		
+	}
+
+}
