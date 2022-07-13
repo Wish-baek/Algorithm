7967d90f98563db7e71be07a149bbfa870adbe41
Wish-baek <dnjs0236@gmail.com>
2022-07-13 14:12:48 +0900

    22.07.13

---
+++ Bronze10409.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Bronze10409 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		int n = sc.nextInt();
+		int t = sc.nextInt();
+		int count = 0;
+		for(int i = 0; i < n; i++) {
+			int a = sc.nextInt();
+			t-=a;
+			if(t<=0) break;
+			count++;
+		}
+		System.out.println(count);
+	}
+
+}
