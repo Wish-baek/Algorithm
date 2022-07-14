0735ca175579133ca66017c7ddef976c7175bb77
Wish-baek <dnjs0236@gmail.com>
2022-07-14 23:41:37 +0900

    22.07.14

--- Bronze10178.java
+++ Bronze10178.java
@@ -5,6 +5,7 @@ public class Bronze10178 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int n = sc.nextInt();
+		
 		for(int i = 0; i < n; i++) {
 			int a = sc.nextInt();
 			int b = sc.nextInt();
