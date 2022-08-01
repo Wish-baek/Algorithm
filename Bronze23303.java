36065f97e3935fb281b5bc84da9516269b4bca26
백소원 <dnjs0236@gmail.com>
2022-08-01 21:34:48 +0900

    22.08.01

---
+++ Bronze23303.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Bronze23303 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		String a = sc.nextLine();
+		
+		if(a.indexOf("d2")>=0 || a.indexOf("D2")>=0) System.out.println("D2");
+		else System.out.println("unrated");
+	}
+
+}
