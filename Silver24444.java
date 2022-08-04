e20774cb2b416d334ca3fcd5f12fa6ce643c5add
백소원 <dnjs0236@gmail.com>
2022-08-04 19:37:07 +0900

    22.08.04

---
+++ Silver24444.java
@@ -0,0 +1,28 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Silver24444 {
+
+	public void bfs(int[] V, int[] E, int[] R) {
+		int[] visited = new int[V.length];
+		for( int v : V ) {
+			
+		}
+	}
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String n = br.readLine();
+		StringTokenizer st = new StringTokenizer(n, " ");
+		int N = Integer.valueOf(st.nextToken());
+		int M = Integer.valueOf(st.nextToken());
+		int R = Integer.valueOf(st.nextToken());
+		
+		
+		
+	}
+
+}
+//https://www.acmicpc.net/problem/24444
