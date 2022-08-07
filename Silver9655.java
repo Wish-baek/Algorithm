9fc31f57bca6340724d0ccddb541d34386a477ba
백소원 <dnjs0236@gmail.com>
2022-08-07 15:17:20 +0900

    22.08.06

---
+++ Silver9655.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Silver9655 {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		
+		int n = sc.nextInt();
+		Boolean check = true;
+		
+		if(n%2==1) System.out.println("SK");
+		else System.out.println("CY");
+		
+	}
+
+}
