25f61c9fe82b7f8e1520b256e34da4e40b08ef43
백소원 <dnjs0236@gmail.com>
2022-08-09 17:03:42 +0900

    22.08.09

---
+++ Bronze2530.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Bronze2530 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int price = sc.nextInt();
+		int t = sc.nextInt();
+		int sum = 0;
+		for(int i = 0; i < t; i++) {
+			sum+=sc.nextInt()*sc.nextInt();
+		}
+		if(sum==price) System.out.println("Yes");
+		else System.out.println("No");
+		
+	}
+
+}
