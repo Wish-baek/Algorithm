5a04557121b933222072597e990f48ad5540e59d
백소원 <dnjs0236@gmail.com>
2022-08-10 21:25:38 +0900

    22.08.10

--- Silver1620.java
+++ Silver1620.java
@@ -1,6 +1,8 @@
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.StringTokenizer;
@@ -10,34 +12,37 @@ public class Silver1620 {
 	public static void main(String[] args) throws NumberFormatException, IOException {
 
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		StringBuilder sb = new StringBuilder();
-		int N = Integer.parseInt(br.readLine());
-		int[] arr = new int[N];
 		StringTokenizer st = new StringTokenizer(br.readLine());
-		for(int i = 0; i < N; i++) {
-			arr[i] = Integer.parseInt(st.nextToken());
+
+		int N = Integer.parseInt(st.nextToken());
+		int M = Integer.parseInt(st.nextToken());
+
+		Map<String, Integer> map = new HashMap<>();
+		String[] str = new String[N+1];
+		
+		for(int i = 1; i <= N; i++) {
+			String name = br.readLine();
+			map.put(name, i);
+			str[i] = name;
 		}
 		
-		
-		Map<Integer, Integer> map = new HashMap<>();
-		int M= Integer.parseInt(br.readLine());
-		int[] arr2 = new int[M];
-		st = new StringTokenizer(br.readLine());
 		for(int i = 0; i < M; i++) {
-			int tmp = Integer.parseInt(st.nextToken());
-			arr2[i] = tmp;
-			map.put(tmp, 0);
-		}
-		for(int i = 0; i < N; i++) {
-			if(map.containsKey(arr[i])) {
-				map.replace(arr[i],map.get(arr[i])+1);
+			String s = br.readLine();
+			if(Character.isDigit(s.charAt(0))) {
+				int index = Integer.parseInt(s);
+				sb.append(str[index]);
 			}
+			else {
+				sb.append(map.get(s));
+			}
+			sb.append("\n");
 		}
-		for( int num : arr2 ) {
-			sb.append(map.get(num)).append(" ");
-		}
+		
 		System.out.println(sb);
+		
 	}
 
 }
-//https://www.acmicpc.net/problem/10816
+//https://www.acmicpc.net/problem/1620
