a048615aa3c6f805c4869b88cd083293d2693a2d
백소원 <dnjs0236@gmail.com>
2022-08-10 22:40:50 +0900

    22.08.10

---
+++ Silver11931.java
@@ -0,0 +1,29 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Comparator;
+
+public class Silver11931 {
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		int N = Integer.parseInt(br.readLine());
+		
+		Integer[] arr = new Integer[N];
+		
+		for(int i = 0; i < N; i++) {
+			arr[i] = Integer.parseInt(br.readLine());
+		}
+		Arrays.sort(arr);
+		Arrays.sort(arr, Comparator.reverseOrder());
+		
+		for( int num : arr )
+			sb.append(num).append("\n");
+		System.out.println(sb);
+	}
+
+
+}
