9d76c6964a1fd092b95d8d5784b81d49ee4f189a
백소원 <dnjs0236@gmail.com>
2022-08-14 10:51:14 +0900

    22.08.12

---
+++ Bronze16968.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+
+public class Bronze16968 {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+
+		String str = sc.next();
+
+		int[] arr = new int[str.length()];
+
+		if (str.charAt(0) == 'c')
+			arr[0] = 26;
+		else if (str.charAt(0) == 'd')
+			arr[0] = 10;
+
+		for (int i = 1; i < str.length(); i++) {
+			if (str.charAt(i - 1) == 'd' && str.charAt(i) == 'd')
+				arr[i] = 9;
+		
+			else if (str.charAt(i - 1) == 'c' && str.charAt(i) == 'c')
+				arr[i] = 25;
+			else if (str.charAt(i) == 'd')
+				arr[i] = 10;
+			else if (str.charAt(i) == 'c')
+				arr[i] = 26;
+		}
+
+		int sum = 1;
+
+		for (int i = 0; i < str.length(); i++) {
+			sum *= arr[i];
+		}
+		System.out.println(sum);
+
+	}
+
+}
+//https://www.acmicpc.net/problem/16968
