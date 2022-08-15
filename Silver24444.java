a5a8bf55f8b0b5f2dbe833d86f8628bdc152b2b6
백소원 <dnjs0236@gmail.com>
2022-08-15 13:55:04 +0900

    22.08.15

--- Silver24444.java
+++ Silver24444.java
@@ -1,26 +1,73 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.LinkedList;
 import java.util.StringTokenizer;
 
 public class Silver24444 {
 
-	public void bfs(int[] V, int[] E, int[] R) {
-		int[] visited = new int[V.length];
-		for( int v : V ) {
-			
+	static ArrayList<ArrayList<Integer>> graph = new ArrayList<>();
+	static int[] check;
+	static int cnt;
+
+	public static void main(String[] args) throws IOException {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+		StringBuilder sb = new StringBuilder();
+
+		int N = Integer.parseInt(st.nextToken()); // 정점의 수
+		int M = Integer.parseInt(st.nextToken()); // 간선의 수
+		int R = Integer.parseInt(st.nextToken()); // 시작 정점
+
+		check = new int[N + 1];
+
+		for (int i = 0; i < N + 1; i++) {
+			graph.add(new ArrayList<Integer>());
 		}
+
+		while (M-- > 0) {
+			st = new StringTokenizer(br.readLine(), " ");
+			int U = Integer.parseInt(st.nextToken());
+			int V = Integer.parseInt(st.nextToken());
+			
+			graph.get(U).add(V);
+			graph.get(V).add(U);
+		}
+		
+		for(int i = 1; i < graph.size(); i++) {
+			Collections.sort(graph.get(i));
+		}
+		
+		cnt = 1;
+		bfs(R);
+		
+		for(int i = 1; i < check.length; i++) {
+			sb.append(check[i]).append("\n");
+		}
+		System.out.println(sb);
+
+
 	}
 	
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		String n = br.readLine();
-		StringTokenizer st = new StringTokenizer(n, " ");
-		int N = Integer.valueOf(st.nextToken());
-		int M = Integer.valueOf(st.nextToken());
-		int R = Integer.valueOf(st.nextToken());
+	static void bfs(int node) {
+		check[node] = cnt;
+		LinkedList<Integer> q = new LinkedList<>();
+		q.offer(node);
 		
-		
+		while(!q.isEmpty()) {
+			int num = q.poll();
+			for(int i = 0; i < graph.get(num).size(); i++) {
+				int newNode = graph.get(num).get(i);
+				if(check[newNode]==0) {
+					cnt++;
+					check[newNode] = cnt;
+					q.offer(newNode);
+				}
+			}
+		}
 		
 	}
 
