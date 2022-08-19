5f90cd4c9629f4066544c0b67c12f087c9562a53
백소원 <dnjs0236@gmail.com>
2022-08-19 09:38:06 +0900

    22.08.19

--- Silver1260.java
+++ Silver1260.java
@@ -1,7 +1,6 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Deque;
 import java.util.LinkedList;
 import java.util.Queue;
 import java.util.StringTokenizer;
