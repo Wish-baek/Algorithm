d6531e984b04b902d75911c67e9c9b4cb3e7141e
백소원 <dnjs0236@gmail.com>
2022-08-21 20:17:27 +0900

    22.08.21

---
+++ Silver1927.java
@@ -0,0 +1,23 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.PriorityQueue;
+
+public class Silver1927 {
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+StringBuilder sb = new StringBuilder();
+		PriorityQueue<Integer> q = new PriorityQueue<>();
+		int n = Integer.parseInt(br.readLine());
+		for(int i = 0; i < n; i++) {
+			int tmp = Integer.parseInt(br.readLine());
+			if(tmp!=0) q.add(tmp);
+			else if(tmp==0 && q.isEmpty())sb.append(0).append("\n");
+			else sb.append(q.poll()).append("\n");
+		}
+		System.out.println(sb);
+	}
+
+}
+//https://www.acmicpc.net/problem/1927
