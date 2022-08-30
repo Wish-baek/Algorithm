be80ebacc3dff7ec0c0a92b181ae391755642402
백소원 <dnjs0236@gmail.com>
2022-08-30 16:51:59 +0900

    22.08.30

---
+++ Bronze2748.java
@@ -0,0 +1,30 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Bronze2748 {
+	static long[] dp;
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		dp = new long[91];
+
+		int n = Integer.parseInt(br.readLine());
+
+		dp[1] = 1;
+		System.out.println(fibo(n));
+	}
+
+	static long fibo(int n) {
+		if (n == 0)
+			return 0;
+		else if (n == 1)
+			return 1;
+		else if (dp[n] > 0)
+			return dp[n];
+		else
+			return dp[n] = fibo(n - 1) + fibo(n - 2);
+	}
+
+}
+//https://www.acmicpc.net/problem/2748
